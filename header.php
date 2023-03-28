b97559147f4143db40a1f27053ffad95b273f448
TheSquid15 <viv@energycluster.dk>
2023-03-28 15:44:56 +0200

    created the sass files and started writing in them

--- header.php
+++ header.php
@@ -18,3 +18,44 @@
         </div>
     </nav>
 </header>
+<style>
+    :root {
+    --main-green: #548235;
+}
+
+* {
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
+body {
+    margin: 0 !important;
+}
+
+nav {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 2rem;
+}
+
+.links_container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.links_container ul{
+    display: flex;
+    align-items: center;
+    list-style: none;
+    padding: 0;
+}
+
+.links_container ul li {
+    margin: 0 2rem;
+    font-weight: bold;
+}
+
+.links_container ul li a {
+    font-style: none;
+}
+</style>
