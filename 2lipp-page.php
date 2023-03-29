c82a1aa8c827568bc2a4de16cab3c49c31becb38
TheSquid15 <viv@energycluster.dk>
2023-03-29 10:35:49 +0200

    renamed the folder

--- 2lipp-page.php
+++ 2lipp-page.php
@@ -3,11 +3,9 @@
 <?php wp_reset_query(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); $currentPostId = $post->ID; ?>
     <div id="wrapper">
-        <?php if(!is_front_page()){ ?>
-          <div id="ecd-wrapper-background"></div>
-        <?php } ?>
-        <?php the_content(); ?>
-        <?php if(is_singular( 'post' )){ include('parts/post-back.php'); } ?>
+        <?php 
+        include("./components/component-banner.php");
+        ?>
     </div>
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
---
+++ components/component-banner.php
@@ -0,0 +1,13 @@
+<?php 
+    $slides = [
+        $slide1 = get_field("slide_1"),
+        $slide2 = get_field("slide_2"),
+        $slide3 = get_field("slide_3"),
+        $slide4 = get_field("slide_4"),
+    ];
+
+    echo '<pre>', var_dump($slides), '</pre>';
+?>
+<div>
+
+</div>
