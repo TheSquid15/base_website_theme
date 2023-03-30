588a1d6187e7accdbe0409387556061b5d930906
TheSquid15 <viv@energycluster.dk>
2023-03-30 15:03:17 +0200

    added the full text scss file

---
+++ components/component-full-text.php
@@ -0,0 +1,14 @@
+<?php
+$text = get_field("whole_text");
+?>
+<div class="container">
+    <div class="formatting">
+        <div class="row">
+            <div class="col-12">
+                <div class="full-text-wrapper">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
