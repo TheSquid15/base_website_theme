56361775d9fcabe8cc5b9bc1b4a72e38f7e57237
TheSquid15 <viv@energycluster.dk>
2023-03-30 17:07:06 +0200

    fixed the column wrappers

--- components/component-testimonials.php
+++ components/component-testimonials.php
@@ -4,28 +4,28 @@
 <div class="container" id="why">
     <div class="formatting">
         <div class="content_container testimonials_container box-green">
-            <div class="row">
+            <div class="row testimonial_row">
                 <div class="col-12 title-wrapper">
                     <h2>Why 2LIPP?</h2>
                 </div>
-                <div class="col-4">
-                    <div class="testimonial_container box-light-green">
+                <div class="col-4 testimonial_container">
+                    <div class="testimonial_wrapper box-light-green">
                         <img />
                         <div class="text_container">
 
                         </div>
                     </div>
                 </div>
-                <div class="col-4">
-                    <div class="testimonial_container box-light-green">
+                <div class="col-4 testimonial_container">
+                    <div class="testimonial_wrapper box-light-green">
                         <img />
                         <div class="text_container">
                             
                         </div>
                     </div>
                 </div>
-                <div class="col-4">
-                    <div class="testimonial_container box-light-green">
+                <div class="col-4 testimonial_container">
+                    <div class="testimonial_wrapper box-light-green">
                         <img />
                         <div class="text_container">
                             
