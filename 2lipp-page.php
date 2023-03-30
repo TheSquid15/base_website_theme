03d1a827f9d7c5a0076e3c0490548b43363cc7a5
TheSquid15 <viv@energycluster.dk>
2023-03-30 17:51:10 +0200

    added a collector file to put all the includes in to make the template file look more clean

--- 2lipp-page.php
+++ 2lipp-page.php
@@ -4,12 +4,7 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); $currentPostId = $post->ID; ?>
     <div id="wrapper">
         <?php 
-        include get_template_directory() . '/components/component-banner.php';
-        include get_template_directory() . '/components/component-picture-text.php';
-        include get_template_directory() . '/components/component-full-text.php';
-        include get_template_directory() . '/components/component-video-container.php';
-        include get_template_directory() . '/components/component-text-picture.php';
-        include get_template_directory() . '/components/component-testimonials.php';
+            include get_template_directory() . '/components/collector.php';
 
         ?>
     </div>
---
+++ components/collector.php
@@ -0,0 +1,10 @@
+<?php 
+        include get_template_directory() . '/components/component-banner.php';
+        include get_template_directory() . '/components/component-picture-text.php';
+        include get_template_directory() . '/components/component-full-text.php';
+        include get_template_directory() . '/components/component-video-container.php';
+        include get_template_directory() . '/components/component-text-picture.php';
+        include get_template_directory() . '/components/component-testimonials.php';
+        include get_template_directory() . '/components/component-funding.php';
+
+?>
