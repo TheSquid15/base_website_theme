ad287faec13f836d0899005c3de43ae902e13ab9
TheSquid15 <viv@energycluster.dk>
2023-03-30 14:33:25 +0200

    started adding the backend elements into the boxes

--- components/component-picture-text.php
+++ components/component-picture-text.php
@@ -1,13 +1,13 @@
 <?php 
-    $picture_left = get_field("picture_left");
-    $text_right = get_field("text_right");
+$picture_left = get_field("picture_left");
+$text_right = get_field("text_right");
 ?>
 <div class="container">
     <div class="formatting">
         <div class="content_container">
             <div class="row text_and_picture container">
                 <div class="col-6 picture_container" style="background-color: red;">
-                    <img src="" />
+                    <img src="<?php echo $picture_left; ?>" />
                 </div>
                 <div class="col-6 text_container" style="background-color: orange;">
                     <p>
