5486010ba983a97e6a650ff95c0726ce053eaf1e
TheSquid15 <viv@energycluster.dk>
2023-03-30 17:33:51 +0200

    added more styling to the boxes and got the content from the backend

--- components/component-testimonials.php
+++ components/component-testimonials.php
@@ -1,5 +1,19 @@
 <?php
+$quotes = [
+    get_field("testimonial_1"),
+    get_field("testimonial_2"),
+    get_field("testimonial_3"),
+];
 
+//echo '<pre>', var_dump($quotes), '</pre>';
+/* 
+array() {
+  array(2) {
+    ["portrait"] => string(69)
+    ["quote"] => string(573)
+  }
+  ...n, n=2
+*/
 ?>
 <div class="container" id="why">
     <div class="formatting">
@@ -8,30 +22,24 @@
                 <div class="col-12 title-wrapper">
                     <h2>Why 2LIPP?</h2>
                 </div>
-                <div class="col-4 testimonial_container">
+                <?php
+                foreach($quotes as $quote) {
+                ?>
+                <div class="col-4 col-xs-12 testimonial_container">
                     <div class="testimonial_wrapper box-light-green">
-                        <img />
+                        <div class="testimonial_portrait">
+                            <img src="<?php echo $quote["portrait"]; ?>" />
+                        </div>
                         <div class="text_container">
-
+                            <?php
+                                echo $quote["quote"];
+                            ?>
                         </div>
                     </div>
                 </div>
-                <div class="col-4 testimonial_container">
-                    <div class="testimonial_wrapper box-light-green">
-                        <img />
-                        <div class="text_container">
-                            
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4 testimonial_container">
-                    <div class="testimonial_wrapper box-light-green">
-                        <img />
-                        <div class="text_container">
-                            
-                        </div>
-                    </div>
-                </div>
+                <?php
+                }
+                ?>
             </div>
         </div>
     </div>
