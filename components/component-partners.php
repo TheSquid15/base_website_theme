a9b1b7fab122823ee186c5e8d32e6e4a7fe2f1b4
TheSquid15 <viv@energycluster.dk>
2023-03-30 18:17:22 +0200

    created a placeholder partner component

--- components/component-partners.php
+++ components/component-partners.php
@@ -8,8 +8,8 @@
                 <div class="col-12">
                     <h2>Partners</h2>
                 </div>
-                <div class="col-12">
-
+                <div class="col-12 partner_logo_container">
+                    <img src="http://localhost/2Lipp2/wp-content/uploads/2023/03/Udklip.png" />
                 </div>
             </div>
         </div>
