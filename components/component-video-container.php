1f1240ba22764dc36f58b6a7fc3369b3ce32e9f5
TheSquid15 <viv@energycluster.dk>
2023-03-30 16:20:14 +0200

    finished the video component

--- components/component-video-container.php
+++ components/component-video-container.php
@@ -17,6 +17,13 @@ $exploded_link = explode("?v=", $youtube_link);
                             <iframe width="834" height="480" src="https://www.youtube.com/embed/<?php echo $exploded_link[1]; ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                         <?php
                         }
+                        else { ?>
+                            <video class="video" loop muted controls preload="auto">
+                                <source src="<?php echo $video; ?>" type="video/mp4">
+                                Your browser does not support the video tag.
+                            </video> 
+                        <?php
+                        }
                         ?>
                     </div>
                 </div>
