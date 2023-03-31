23597d75865d20c4dd548c2dd3ea7152372e4bd3
TheSquid15 <viv@energycluster.dk>
2023-03-31 09:05:20 +0200

    did some minor tweaks to the grid system

--- components/component-partners.php
+++ components/component-partners.php
@@ -9,7 +9,7 @@
                     <h2>Partners</h2>
                 </div>
                 <div class="col-12 partner_logo_container">
-                    <img src="http://localhost/2Lipp2/wp-content/uploads/2023/03/Udklip.png" />
+                    <img src="https://2lipp.eu/home/wp-content/uploads/2023/03/Udklip.png" />
                 </div>
             </div>
         </div>
