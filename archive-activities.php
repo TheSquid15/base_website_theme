69630acbd8ebfc69be2f617100c4b1feeea8a407
TheSquid15 <viv@energycluster.dk>
2023-04-14 10:49:27 +0200

    started adding stylesheets for the activities and created a archive page

--- archive-activities.php
+++ archive-activities.php
@@ -1,4 +1,18 @@
-<?php
-echo "Hello there!";
-?>
-<h1>HELLO!</h1>
+<?php get_header(); ?>
+<?php wp_reset_query(); ?>
+<div class="container">
+    <div class="formatting">
+        <div class="content_container">
+            <div class="row">
+                <?php if(have_posts()) { ?>
+                <div class="col-4 col-md-12 activity_container">
+
+                </div>
+                <?php
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
