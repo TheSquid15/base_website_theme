02901c242700e9df43a84ce449708dbae5138396
TheSquid15 <viv@energycluster.dk>
2023-04-14 11:31:06 +0200

    started working on the loop

--- archive-activities.php
+++ archive-activities.php
@@ -1,18 +1,22 @@
 <?php get_header(); ?>
 <?php wp_reset_query(); ?>
+<?php if(have_posts()) : ?>
 <div class="container">
     <div class="formatting">
         <div class="content_container">
             <div class="row">
-                <?php if(have_posts()) { ?>
+                <?php while(have_posts()) : the_post(); $currentPostID = $post->ID; ?>
                 <div class="col-4 col-md-12 activity_container">
+                    <div class="activity-image-container">
 
+                    </div>
+                    <div class="text_container">
+
+                    </div>
                 </div>
-                <?php
-                }
-                ?>
             </div>
         </div>
     </div>
 </div>
+<?php endif; ?>
 <?php get_footer(); ?>
