d23f092b87a5a7185d857828b19fcedcab9c5e3e
TheSquid15 <viv@energycluster.dk>
2023-04-14 11:41:59 +0200

    made sure that the loop works

--- archive-activities.php
+++ archive-activities.php
@@ -4,11 +4,11 @@
 <div class="container">
     <div class="formatting">
         <div class="content_container">
-            <div class="row">
+            <div class="row activities-archive-row">
                 <?php while(have_posts()) : the_post(); $currentPostID = $post->ID; ?>
                 <div class="col-4 col-md-12 activity_container">
                     <div class="activity-image-container">
-
+                        <h3><?php echo get_the_title(); ?></h3>
                     </div>
                     <div class="text_container">
 
