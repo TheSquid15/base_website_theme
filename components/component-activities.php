e2b1d71c98a3edf728158ef5a71323b5a4e796f8
TheSquid15 <viv@energycluster.dk>
2023-04-17 10:26:43 +0200

    started working on the carousel

--- components/component-activities.php
+++ components/component-activities.php
@@ -7,16 +7,31 @@ $args = array(
     'post_type' => 'activities'
 );
 
+$all_activities = get_posts($args);
+
 ?>
-<div id="activities" class="container">
+<div id="activities" class="container box-green">
     <div class="formatting">
         <div class="content_container">
-            <div class="row partners_row">
+            <div class="row activities_row">
                 <div class="col-12 title-wrapper">
                     <h2>Activities</h2>
                 </div>
-                <div class="col-12">
+                <div class="col-12 kiosk">
+                    <div class="kiosk-container">
+                        <ul class="kiosk-activities">
+                            <li><img src="" alt=""></li>
+                            <li><img src="" alt=""></li>
+                            <li><img src="" alt=""></li>
+                            <li><img src="" alt=""></li>
+                            <li><img src="" alt=""></li>
+                            <li><img src="" alt=""></li>
 
+                        </ul>
+                    </div>
+                    <?php
+                    echo '<pre>', var_dump($all_activities), '</pre>';
+                    ?>
                 </div>
             </div>
         </div>
