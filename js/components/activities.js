1c625a58366e80c40f9e66ef74fd9f959dfef61d
TheSquid15 <viv@energycluster.dk>
2023-04-17 11:33:35 +0200

    added a function to move the boxes by a certain amount

--- js/components/activities.js
+++ js/components/activities.js
@@ -1,10 +1,16 @@
 // Get all the elements with the activity class
-const activities = document.getElementsByClassName("activity");
+const activities = document.querySelectorAll(".activity");
+
+console.log(activities);
 
 let currentOffset = 0;
 
 let maxActivities = activities.length - 1;
 
 function transformActivities() {
-    
+    activities.forEach((box, index) => {
+        box.style.transform = `translateX(calc(${-100 * (currentOffset)}%))`;
+    });
 }
+
+transformActivities();
