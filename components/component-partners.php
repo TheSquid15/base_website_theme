3e08d8e2d2ff65e5c7578c1b07942a5200308747
TheSquid15 <viv@energycluster.dk>
2023-04-21 12:58:27 +0200

    fixed the grid system so it applies the right styling for the appropriate break points

--- components/component-partners.php
+++ components/component-partners.php
@@ -1,5 +1,14 @@
 <?php
+wp_reset_query();
 
+$args = array(
+    'post_type' => 'partners',
+    'posts_per_page' => -1,
+    'post_status' => 'publish',
+    'order_by' => 'rand'
+);
+
+$query = new WP_Query($args);
 ?>
 <div id="partners" class="container">
     <div class="formatting">
@@ -8,9 +17,23 @@
                 <div class="col-12 title-wrapper">
                     <h2>Partners</h2>
                 </div>
-                <div class="col-12 partner_logo_container">
-                    <img src="https://2lipp.eu/home/wp-content/uploads/2023/03/Udklip.png" />
+                <?php
+                //echo '<pre>', var_dump($query), '</pre>';
+                if( $query->have_posts() ):
+                    while ($query->have_posts()): $query->the_post(); $id = get_the_ID();
+                    $logo = get_field("partner_logo", $id);
+                ?>
+                <div class="partner-logo-container col-3 col-small-12 col-md-6 col-large-4">
+                    <?php if(isset($logo)): ?>
+                    <img class="partner-logo" src="<?php echo $logo; ?>" />
+                    <?php
+                    endif;
+                    ?>
                 </div>
+                <?php
+                endwhile;
+                endif;
+                ?>
             </div>
         </div>
     </div>
